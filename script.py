bebda7b7f8f6c7e213351e1f7378cc95d0ffe507
binayakbishnu <bishnu.binayak12@gmail.com>
2023-07-30 11:18:24 +0530

    dataframe added

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+    print('Opened file')
+
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data = d)
 
 
 
-print('Open file')
+
+
+    print("Script completed successfully")
