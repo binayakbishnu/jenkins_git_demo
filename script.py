988033deee13941294200b2927157de7f98cfb9d
binayakbishnu <bishnu.binayak12@gmail.com>
2023-07-30 14:00:23 +0530

    removed pandas

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
-import pandas as pd
+# import pandas as pd
 
 
 if __name__ == '__main__':
     print('Opened file')
 
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data = d)
+    # d = {'col1': [1, 2], 'col2': [3, 4]}
+    # df = pd.DataFrame(data = d)
 
 
 
